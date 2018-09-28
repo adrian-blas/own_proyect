9a9a548ecc7966a2afa957bdab031d3cdb24ab8c
militho <militho.blazitho@gmail.com>
2018-09-28 17:33:09 -0500

    add firebase plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Config
+import { firebaseConfig } from './config/config';
+
 // Rutas
 import { APP_ROUTES } from './app.routes';
 
@@ -20,6 +23,14 @@ import { RegisterComponent } from './login/register.component';
 import { PagesComponent } from './pages/pages.component';
 import { SharedModule } from './shared/shared.module';
 
+// Firebase
+// Documentacion para instalar
+// https://github.com/angular/angularfire2/blob/master/docs/install-and-setup.md
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,11 +41,14 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     APP_ROUTES,
-    // PagesModule,
     FormsModule,
     ReactiveFormsModule,
     ServiceModule,
-    SharedModule
+    SharedModule,
+    AngularFireModule.initializeApp( firebaseConfig ), // imports firebase/app needed for everything
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule // imports firebase/storage only needed for storage features
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,3 +1,10 @@
+export const URL_SERVICIOS = 'https://yapsi-pay.firebaseio.com';
 
-
-export const URL_SERVICIOS = 'http://localhost:3000';
+export const firebaseConfig = {
+  apiKey: "AIzaSyBMHBVCGKwSlBWdTsnqkTaLC5NnxsaHTUI",
+  authDomain: "own-proyect-65664.firebaseapp.com",
+  databaseURL: "https://own-proyect-65664.firebaseio.com",
+  projectId: "own-proyect-65664",
+  storageBucket: "own-proyect-65664.appspot.com",
+  messagingSenderId: "354497899176"
+}
